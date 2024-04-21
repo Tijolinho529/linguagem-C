7e75ed709e7110f4599b976efed13f6df07b4f2a
Tijolinho529 <161033643+Tijolinho529@users.noreply.github.com>
2024-04-21 13:21:47 -0300

    Add files via upload

---
+++ atividade da pessoa obrigada a votar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	//Variáveis
+	unsigned int idade;
+	
+	//Dados do usuário
+	printf("Digite a sua idade: ");
+	scanf("%i", &idade);
+	
+	if(idade < 18 || idade > 65){
+		printf("Você não é obrigado a votar");
+	}else{
+		printf("Você é obrigado a votar");
+	}
+	
+	return 0;
+	}
